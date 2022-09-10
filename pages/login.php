2073f7c055d26dbb7c24dba5d0c6c42e34e36e08
VanyaShumilo <ivan.shumilo@gmail.com>
2022-09-11 01:00:26 +0300

    bug fix

--- pages/login.php
+++ pages/login.php
@@ -17,7 +17,7 @@ require '../scripts/paths.php'
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../libs/_bootstrap-reboot.min.css">
     <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="../style/mainPage.css">
+    <!-- <link rel="stylesheet" href="../style/mainPage.css"> -->
     <title>Login</title>
 </head>
 
